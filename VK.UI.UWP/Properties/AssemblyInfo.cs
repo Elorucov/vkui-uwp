602468f735a294d2da30c69edd6e0c22cab6527f
Elorucov <elorucov@live.ru>
2019-12-24 12:48:34 +0400

    Updated icons.

--- VK.UI.UWP/Properties/AssemblyInfo.cs
+++ VK.UI.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VK.UI.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VKUI library")]
+[assembly: AssemblyDescription("VKUI library for UWP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("VKontakte")]
 [assembly: AssemblyProduct("VK.UI.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 - 2020 Elorucov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
 [assembly: ComVisible(false)]
