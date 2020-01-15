aec4ff573c0c3d7d2410b1b1d4ee1f15c8738760
Elorucov <elorucov@live.ru>
2020-01-15 21:33:11 +0400

    Fixes.

---
+++ VK.UI.UWP/Controls/VKUIMenuFlyout.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace VK.UI.UWP.Controls
+{
+    public sealed class VKUIMenuFlyout : FlyoutBase
+    {
+        public VKUIMenuFlyout() {
+            
+        }
+
+        protected override Control CreatePresenter() {
+            MenuFlyoutPresenter mfp = new MenuFlyoutPresenter();
+            mfp.ItemsSource = new List<string> { "Test", "Tost" };
+            return mfp;
+        }
+    }
+}
