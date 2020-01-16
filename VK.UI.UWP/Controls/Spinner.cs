211012eb7dfad8677f78a3e81edc8a0ec0b45a5b
Elorucov <elorucov@live.ru>
2020-01-16 11:24:35 +0400

    Spinner control.

---
+++ VK.UI.UWP/Controls/Spinner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace VK.UI.UWP.Controls {
+    public sealed class Spinner : Control {
+
+        #region Template elements
+
+        ContentPresenter SpinnerContainer;
+        RotateTransform SpinnerRotator;
+
+        #endregion
+
+        public Spinner() {
+            this.DefaultStyleKey = typeof(Spinner);
+        }
+
+        protected override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+            SpinnerContainer = (ContentPresenter)GetTemplateChild(nameof(SpinnerContainer));
+            SpinnerRotator = SpinnerContainer.RenderTransform as RotateTransform;
+            SizeChanged += OnSizeChanged;
+            Unloaded += (a, b) => SizeChanged -= OnSizeChanged;
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e) {
+            double s = Math.Min(e.NewSize.Width, e.NewSize.Height);
+            double ss = GetSpinnerSize(s);
+            SpinnerRotator.CenterX = ss / 2;
+            SpinnerRotator.CenterY = ss / 2;
+            SpinnerContainer.ContentTemplate = Application.Current.Resources[$"Icon{ss}Spinner"] as DataTemplate;
+        }
+
+        private int GetSpinnerSize(double s) {
+            if (s >= 38) return 44;
+            if (s >= 28) return 32;
+            if (s >= 20) return 24;
+            return 16;
+        }
+    }
+}
