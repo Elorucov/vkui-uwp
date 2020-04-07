6d3e9cac41e08cdeb6f2b4251f46fcdf8f5f1842
Elorucov <elorucov@live.ru>
2020-04-07 21:23:59 +0400

    Now MenuFlyout supports these controls: RadioButton, MenuFlyoutItem and MenuFlyoutSeparator.

--- VK.UI.UWP/Flyouts/MenuFlyout.cs
+++ VK.UI.UWP/Flyouts/MenuFlyout.cs
@@ -11,9 +11,21 @@ using Windows.UI.Xaml.Controls.Primitives;
 
 namespace VK.UI.UWP.Flyouts {
     public class MenuFlyout : FlyoutBase {
-        public ObservableCollection<CellButton> Items { get; set; } = new ObservableCollection<CellButton>();
+        public ObservableCollection<Control> Items { get; set; } = new ObservableCollection<Control>();
 
-        public MenuFlyout() { }
+        public MenuFlyout() {
+            Closed += (d, e) => {
+                foreach(Control c in Items) {
+                    if(c is CellButton cb) {
+                        cb.Click -= (a, b) => Hide();
+                    } else if(c is MenuFlyoutItem mfi) {
+                        mfi.Click -= (a, b) => Hide();
+                    } else if(c is RadioButton rb) {
+                        rb.Click -= (a, b) => Hide();
+                    }
+                }
+            };
+        }
 
         protected override Control CreatePresenter() {
             ScrollViewer sv = new ScrollViewer {
@@ -21,10 +33,25 @@ namespace VK.UI.UWP.Flyouts {
             };
 
             StackPanel sp = new StackPanel();
-            foreach(CellButton cb in Items) {
-                if (cb.Style == null) cb.Style = (Style)Application.Current.Resources["MenuFlyoutCellButtonStyle"];
-                cb.Click += (a, b) => Hide();
-                sp.Children.Add(cb);
+            foreach(Control c in Items) {
+                if (c is CellButton cb) {
+                    if (cb.Style == null) cb.Style = (Style)Application.Current.Resources["MenuFlyoutCellButtonStyle"];
+                    cb.Click += (a, b) => Hide();
+                    sp.Children.Add(cb);
+                } else if (c is MenuFlyoutItem mfi) {
+                    if (mfi.Style == null) mfi.Style = (Style)Application.Current.Resources["VKUIMenuFlyoutItemStyle"];
+                    mfi.Click += (a, b) => Hide();
+                    sp.Children.Add(mfi);
+                } else if (c is RadioButton rb) {
+                    if (rb.Style == null) rb.Style = (Style)Application.Current.Resources["VKUIRadioButtonForFlyoutStyle"];
+                    rb.Click += (a, b) => Hide();
+                    sp.Children.Add(rb);
+                } else if (c is MenuFlyoutSeparator mfs) {
+                    if (mfs.Style == null) mfs.Style = (Style)Application.Current.Resources["VKUIMenuFlyoutSeparatorStyle"];
+                    sp.Children.Add(mfs);
+                } else {
+                    throw new ArgumentException($"Items must contain only objects of these types: {nameof(CellButton)}, {nameof(MenuFlyoutItem)} and {nameof(MenuFlyoutSeparator)}");
+                }
             }
             sv.Content = sp;
 
